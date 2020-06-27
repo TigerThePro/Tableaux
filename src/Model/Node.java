724488cf1d4d530ac4cd2e67ee840d2cea898972
j1o2b <tigerthepro@gmail.com>
2020-06-26 21:49:35 -0700

    spec added for Node

---
+++ src/Model/Node.java
@@ -0,0 +1,11 @@
+package Model;
+
+public class Node {
+    // Node has a list of sentence in the order added
+    // anoter list of sentence in the order of priority
+    //       1 comes before 2, 0 NOT in the list
+    // has children
+    // children can be added
+    // is either open or closed, a closed one cannot have children
+
+}
